f4f3f867af69c482930b7a4157e61901dec7e278
root <tesfahunfola>
2023-07-16 13:24:54 +0000

    update

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,40 +1,46 @@
 #!/usr/bin/python3
-"""
-Recursive function to count keywords from hot articles on Reddit.
-"""
+"""Module for the count_words function"""
 
 import requests
 
 
-def count_words(subreddit, word_list, after='', counts=None):
-    if counts is None:
-        counts = {}
-        word_list = [word.lower() for word in word_list]
-
-    if after is None:
-        sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
-        for word, count in sorted_counts:
-            if count > 0:
-                print(f"{word}: {count}")
-        return
+def count_words(subreddit, word_list, after='', hot_list=None):
+    """
+    Recursive function that queries the Reddit API, parses the title of all hot articles,
+    and prints a sorted count of given keywords.
+    """
+    if hot_list is None:
+        hot_list = [0] * len(word_list)
 
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
     params = {'limit': 100, 'after': after}
-    response = requests.get(url, headers=headers, params=params)
 
-    if response.status_code == 200:
-        data = response.json()
-        children = data['data']['children']
-        for child in children:
-            title = child['data']['title'].lower().split()
-            for word in word_list:
-                if word in title:
-                    counts[word] = counts.get(word, 0) + 1
-        after = data['data']['after']
-        count_words(subreddit, word_list, after, counts)
+    try:
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"Error occurred during API request: {e}")
+        return
+
+    data = response.json()
+
+    for topic in data['data']['children']:
+        for word in topic['data']['title'].split():
+            for i in range(len(word_list)):
+                if word_list[i].lower() == word.lower():
+                    hot_list[i] += 1
+
+    after = data['data']['after']
+
+    if after is not None:
+        count_words(subreddit, word_list, after, hot_list)
     else:
-        print("Invalid subreddit or no posts match.")
+        sorted_counts = sorted(zip(word_list, hot_list), key=lambda x: (-x[1], x[0]))
+
+        for word, count in sorted_counts:
+            if count > 0:
+                print(f"{word.lower()}: {count}")
 
 
 if __name__ == '__main__':
