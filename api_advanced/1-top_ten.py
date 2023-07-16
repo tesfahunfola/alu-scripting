66ec5605ae75e634fd0e17047f39d80bae6e7587
root <tesfahunfola>
2023-07-16 12:31:51 +0000

    update

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,4 +1,8 @@
-s module contains a function that queries the Reddit API and prints the
+#!/usr/bin/python3
+"""
+1-top_ten.py: Top Ten
+
+This module contains a function that queries the Reddit API and prints the
 titles of the first 10 hot posts listed for a given subreddit.
 """
 
